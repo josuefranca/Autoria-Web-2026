91114408e6ae1bc5c5ff7497086ad4f6257b0df0
Josué Antônio de Oliveira França <20261074010015@ifrn.local>
2026-09-03 14:21:24 -0300

    criando commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Javascript funcionando");
+
+const titulo = document.getElementById("titulo");
+const mensagem = document.getElementById("mensagem");
+const botao = document.getElementById("botao");
+const remover = document.getElementById("remover");
+const alterar = document.getElementById("alterar");
+
+botao.addEventListener("click", function (){
+    mensagem.classList.add("destaque");
+});
+
+remover.addEventListener("click", function (){
+    mensagem.classList.toggle("destaque");
+
+
+});
